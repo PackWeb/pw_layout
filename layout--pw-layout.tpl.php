a8d2260070c2b187cef2acae68b2f44cc33ea8b2
Peter Anderson <peter@panda.id.au>
2019-09-11 21:14:57 +0300

    Change to flexbox and allow full-width regions.

--- layout--pw-layout.tpl.php
+++ layout--pw-layout.tpl.php
@@ -7,60 +7,68 @@
 
 <div class="layout--pw-layout <?php print implode(' ', $classes); ?>"<?php print backdrop_attributes($attributes); ?>>
 
-  <a href="#main-content" class="skip-link element-focusable">
-    <?php print t('Skip to main content'); ?>
-  </a>
+  <a class="skip-link element-focusable" href="#main-content"><?php print t('Skip to main content'); ?></a>
 
-  <?php if ($content['header_top']): ?>
-    <header class="l-header-top"><div class="l-inner">
-      <?php print $content['header_top']; ?>
-    </div></header>
-  <?php endif; ?>
+  <?php if ($content['header_top'] || $content['header_bottom']): ?>
+    <header class="l-header">
+      <?php if ($content['header_top']): ?>
+        <div class="l-header-top">
+          <div class="l-inner"><?php print $content['header_top']; ?></div>
+        </div>
+      <?php endif; ?>
 
-  <?php if ($content['header_bottom']): ?>
-    <div class="l-header-bottom"><div class="l-inner">
-      <?php print $content['header_bottom']; ?>
-    </div></div>
+      <?php if ($content['header_bottom']): ?>
+        <div class="l-header-bottom">
+          <div class="l-inner"><?php print $content['header_bottom']; ?></div>
+        </div>
+      <?php endif; ?>
+    </header>
   <?php endif; ?>
 
   <?php if ($content['above']): ?>
-    <div class="l-above"><div class="l-inner">
-      <?php print $content['above']; ?>
-    </div></div>
+    <div class="l-above">
+      <div class="l-inner"><?php print $content['above']; ?></div>
+    </div>
   <?php endif; ?>
 
-  <main id="main-content" class="l-content"><div class="l-inner">
-    <?php print $content['content']; ?>
-  </div></main>
+  <div class="l-main <?php print implode(' ', $main_classes); ?>">
+    <main class="l-content" id="main-content">
+      <div class="l-inner"><?php print $content['content']; ?></div>
+    </main>
 
-  <?php if ($content['sidebar_left']): ?>
-    <aside class="l-sidebar-left"><div class="l-inner">
-      <?php print $content['sidebar_left']; ?>
-    </div></aside>
-  <?php endif; ?>
+    <?php if ($content['sidebar_left']): ?>
+      <div class="l-sidebar-left">
+        <div class="l-inner"><?php print $content['sidebar_left']; ?></div>
+      </div>
+    <?php endif; ?>
 
-  <?php if ($content['sidebar_right']): ?>
-    <aside class="l-sidebar-right"><div class="l-inner">
-      <?php print $content['sidebar_right']; ?>
-    </div></aside>
-  <?php endif; ?>
+    <?php if ($content['sidebar_right']): ?>
+      <div class="l-sidebar-right">
+        <div class="l-inner"><?php print $content['sidebar_right']; ?></div>
+      </div>
+    <?php endif; ?>
+  </div>
 
   <?php if ($content['below']): ?>
-    <div class="l-below"><div class="l-inner">
-      <?php print $content['below']; ?>
-    </div></div>
+    <div class="l-below">
+      <div class="l-inner"><?php print $content['below']; ?></div>
+    </div>
   <?php endif; ?>
 
-  <?php if ($content['footer_top']): ?>
-    <div class="l-footer-top"><div class="l-inner">
-      <?php print $content['footer_top']; ?>
-    </div></div>
-  <?php endif; ?>
+  <?php if ($content['footer_top'] || $content['footer_bottom']): ?>
+    <footer class="l-footer">
+      <?php if ($content['footer_top']): ?>
+        <div class="l-footer-top">
+          <div class="l-inner"><?php print $content['footer_top']; ?></div>
+        </div>
+      <?php endif; ?>
 
-  <?php if ($content['footer_bottom']): ?>
-    <footer class="l-footer-bottom"><div class="l-inner">
-      <?php print $content['footer_bottom']; ?>
-    </div></footer>
+      <?php if ($content['footer_bottom']): ?>
+        <div class="l-footer-bottom">
+          <div class="l-inner"><?php print $content['footer_bottom']; ?></div>
+        </div>
+      <?php endif; ?>
+    </footer>
   <?php endif; ?>
 
 </div>
--- pw_layout.php
+++ pw_layout.php
@@ -8,18 +8,9 @@
  * Process variables for the PackWeb layout.
  */
 function template_preprocess_layout__pw_layout(&$variables) {
-  $left = $variables['content']['sidebar_left'];
-  $right = $variables['content']['sidebar_right'];
-
-  // Add classes to the layout to indicate which (if any) sidebars are in use.
-  if ($left && $right) {
-    $variables['classes'][] = 'sidebar-both';
-  }
-  elseif ($left) {
-    $variables['classes'][] = 'sidebar-left';
-  }
-  elseif ($right) {
-    $variables['classes'][] = 'sidebar-right';
+  // Add class if sidebars are in use.
+  if ($variables['content']['sidebar_left'] || $variables['content']['sidebar_right']) {
+    $variables['main_classes'][] = 'has-sidebar';
   }
 }
 
